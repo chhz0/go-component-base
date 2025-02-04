c1158e8ba1255a987f2c51ac10bdcc93e7740f3a
ch <ch1447571882@qq.com>
2025-02-04 21:51:48 +0800

    feat(meta): 新增元数据和字段选择功能
    - 添加 ObjectMeta、TypeMeta 等元数据结构
    - 实现字段选择器和条件匹配功能
    - 添加扩展属性处理和 JSON 序列化/反序列化支持
    - 新增列表选项、导出选项等辅助结构

---
+++ meta/fields/requirement.go
@@ -0,0 +1,11 @@
+package fields
+
+import "github.com/chhz0/go-component-base/meta/selection"
+
+type Requirements []Requirement
+
+type Requirement struct {
+	Operator selection.Operator
+	Field    string
+	Value    string
+}
